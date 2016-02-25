93886254862053b6c0f78d276f043d136e038863
Jeff Doolittle <jeffdoolittle@gmail.com>
2016-02-25 02:42:18 -0800

    wip

---
+++ src/Ferret.Sample/Domain/Mischief.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Ferret.Sample.Domain
+{
+    public class Mischief
+    {
+        public class Manager
+        {
+            private readonly IAggregateRepository _repository;
+
+            public Manager(IAggregateRepository repository)
+            {
+                _repository = repository;
+            }
+
+            public void When(OpenMaraudersMap command)
+            {
+                var aggregate = _repository.Load<Aggregate, State>(command.MischiefId);
+                aggregate.When(command);
+                _repository.Save(aggregate);
+            }
+
+            public void When(GotoRoomOfRequirement command)
+            {
+                var aggregate = _repository.Load<Aggregate, State>(command.MischiefId);
+                aggregate.When(command);
+                _repository.Save(aggregate);
+            }
+        }
+
+        public class State : StateBase
+        {
+            public int TimesVisitedRoomOfRequirement { get; set; }
+
+            public void Apply(OpenedMaraudersMap e)
+            {
+                Id = e.MischiefId;
+                Version += 1;
+            }
+
+            public void Apply(WentToRoomOfRequirement e)
+            {
+                TimesVisitedRoomOfRequirement += 1;
+                Version += 1;
+            }
+        }
+
+        public class Aggregate : Aggregate<State>
+        {
+            public Aggregate(State state)
+                : base(state)
+            {
+            }
+
+            public void When(OpenMaraudersMap command)
+            {
+                Apply(new OpenedMaraudersMap { MischiefId = command.MischiefId });
+            }
+
+            public void When(GotoRoomOfRequirement command)
+            {
+                Apply(new WentToRoomOfRequirement { MischiefId = command.MischiefId });
+            }
+        }
+    }
+
+    public class OpenMaraudersMap
+    {
+        public Guid MischiefId { get; set; }
+    }
+
+    public class OpenedMaraudersMap
+    {
+        public Guid MischiefId { get; set; }
+    }
+
+    public class GotoRoomOfRequirement
+    {
+        public Guid MischiefId { get; set; }
+    }
+
+    public class WentToRoomOfRequirement
+    {
+        public Guid MischiefId { get; set; }
+    }
+}
---
+++ src/Ferret.Sample/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Npgsql;
+using Marten;
+using Topshelf;
+using Topshelf.Nancy;
+using Nancy;
+using log4net.Config;
+using Nancy.TinyIoc;
+using Ferret.Sample.Domain;
+using Nancy.Bootstrapper;
+using log4net;
+using Nancy.ModelBinding;
+
+namespace Ferret.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            if (args != null && args.Any(x => x.ToLowerInvariant() == "--init"))
+            {
+                string target = GetTargetConnectionString();
+                string master = GetMasterConnectionString(target);
+
+                var store = new Initializer(master, target).Initialize(ConfigureSchema);
+
+                return;
+            }
+            else if (args != null && args.Any(x => x.ToLowerInvariant() == "--tear-down"))
+            {
+                string target = GetTargetConnectionString();
+                string master = GetMasterConnectionString(target);
+
+                var store = new Initializer(master, target).Initialize(ConfigureSchema);
+
+                store.Advanced.Clean.CompletelyRemoveAll();
+                return;
+            }
+
+            var host = HostFactory.New(x =>
+            {
+                x.UseLog4Net();
+
+                x.Service<SampleService>(s =>
+                {
+                    s.ConstructUsing(settings => new SampleService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                    s.WithNancyEndpoint(x, c =>
+                    {
+                        c.AddHost(port: 8585);
+                        c.CreateUrlReservationsOnInstall();
+                    });
+                });
+                x.StartAutomatically();
+                x.SetServiceName("Ferret.Sample");
+                x.RunAsNetworkService();
+            });
+
+            host.Run();
+        }
+
+        public static string GetMasterConnectionString(string target)
+        {
+            var builder = new NpgsqlConnectionStringBuilder(target);
+            builder.Database = "postgres";
+            var master = builder.ConnectionString;
+            return master;
+        }
+
+        public static string GetTargetConnectionString()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "connection.txt");
+            if (!File.Exists(path))
+            {
+                var dir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+                path = Path.Combine(dir.Parent.Parent.FullName, "connection.txt");
+            }
+
+            var target = File.ReadLines(path).First();
+            return target;
+        }
+
+        static void ConfigureSchema(MartenRegistry cfg)
+        {
+
+        }
+    }
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            string target = Program.GetTargetConnectionString();
+
+            var store = DocumentStore.For(target);
+
+            container.Register<IDocumentStore>(store);
+        }
+
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            base.ConfigureRequestContainer(container, context);
+
+            var store = container.Resolve<IDocumentStore>();
+            var repository = new AggregateRepository(store);
+
+            container.Register<IAggregateRepository>(repository);
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.BeforeRequest += (NancyContext ctx) =>
+            {
+                var log = LogManager.GetLogger("Nancy.BeforeRequest");
+                log.InfoFormat("[{0}] {1}", ctx.Request.Method, ctx.Request.Url);
+                return null;
+            };
+            pipelines.AfterRequest += (NancyContext ctx) =>
+            {
+                var log = LogManager.GetLogger("Nancy.AfterRequest");
+                log.InfoFormat("{0} [{1}] {2}", (int)ctx.Response.StatusCode, ctx.Request.Method, ctx.Request.Url);
+            };
+            pipelines.OnError += (ctx, ex) =>
+            {
+                var log = LogManager.GetLogger("Nancy.OnError");
+                log.ErrorFormat("[{0}] {1}\r\n{2}", ctx.Request.Method, ctx.Request.Url, ex);
+                return null;
+            };
+        }
+    }
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                return "Hello Ferret!";
+            };
+        }
+    }
+
+    public class MischiefModule : NancyModule
+    {
+        public MischiefModule(Mischief.Manager manager)
+            : base("/mischief")
+        {
+            Get["/{mischiefId}"] = _ =>
+            {
+
+                return "Mischief status";
+            };
+
+            Post["/map-discovery"] = _ =>
+            {
+                var command = this.Bind<OpenMaraudersMap>();
+                manager.When(command);
+                return HttpStatusCode.OK;
+            };
+
+            Post["/room-of-requirement-visit"] = _ =>
+            {
+                var command = this.Bind<GotoRoomOfRequirement>();
+                manager.When(command);
+                return HttpStatusCode.OK;
+            };
+        }
+    }
+
+    public class SampleService
+    {
+        public bool Start()
+        {
+            return true;
+        }
+        public bool Stop()
+        {
+            return true;
+        }
+    }
+}
